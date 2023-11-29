381ffe6ac0615603f611279218aee29adc0d1779
DeVeNdRa1910 <drvishwakarma1910@gmail.com>
2023-11-29 11:46:37 +0530

    add list Item

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var item = document.getElementsByClassName('list-group-item');
 item[2].style.backgroundColor = "green"
-for(let i = 0 ; i < 4 ; i++){
+for(let i = 0 ; i < item.length ; i++){
     item[i].style.fontWeight = 'bold';
 }
+
+function addLiItem( liContent ){
+    const list = document.createElement('li')
+    list.appendChild(document.createTextNode(liContent))
+    document.querySelector('.list-group').appendChild(list)
+}
+
+addLiItem('Item 5')
